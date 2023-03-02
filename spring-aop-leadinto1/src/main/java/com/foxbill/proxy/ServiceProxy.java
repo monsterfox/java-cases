42d50810d7274ebc0b7cdbcbcfd1d6201844e71b
foxbill <565197135@qq.com>
2023-03-02 10:51:31 +0800

    Aop概念引入

---
+++ spring-aop-leadinto1/src/main/java/com/foxbill/proxy/ServiceProxy.java
@@ -0,0 +1,25 @@
+package com.foxbill.proxy;
+
+import com.foxbill.service.SomeService;
+import com.foxbill.service.impl.SomeServiceImpl;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ServiceProxy implements SomeService{
+
+    SomeService someService = new SomeServiceImpl();
+
+    @Override
+    public void doSome(){
+        System.out.println("非业务功能，日志功能：在方法开始时输出日志");
+        someService.doSome();
+        System.out.println("非业务功能，事务功能：在业务方法执行之后，加入事务");
+    }
+
+    @Override
+    public void doOther() {
+        System.out.println("非业务功能，日志功能：在方法开始时输出日志");
+        someService.doOther();
+        System.out.println("非业务功能，事务功能：在业务方法执行之后，加入事务");
+    }
+}
---
+++ spring-aop-leadinto1/src/main/java/com/foxbill/service/SomeService.java
@@ -0,0 +1,6 @@
+package com.foxbill.service;
+
+public interface SomeService {
+    void doSome();
+    void doOther();
+}
