b0f2609a28acfa7289e1a9899eb071db450cf3a5
foxbill <565197135@qq.com>
2023-03-08 07:27:50 +0800

    Spring-事务-无事务状态演示

---
+++ spring-trans-sale/src/main/java/com/foxbill/service/impl/GoodsServiceImpl.java
@@ -0,0 +1,43 @@
+package com.foxbill.service.impl;
+
+import com.foxbill.dao.GoodsDao;
+import com.foxbill.dao.SaleDao;
+import com.foxbill.domain.Goods;
+import com.foxbill.domain.Sale;
+import com.foxbill.exception.NotEnoughException;
+import com.foxbill.service.GoodsService;
+
+public class GoodsServiceImpl implements GoodsService {
+
+    private GoodsDao goodsDao;
+    private SaleDao saleDao;
+
+    public void setGoodsDao(GoodsDao goodsDao) {
+        this.goodsDao = goodsDao;
+    }
+
+    public void setSaleDao(SaleDao saleDao) {
+        this.saleDao = saleDao;
+    }
+
+    @Override
+    public void buy(Integer goodsId, Integer amount) {
+        Sale sale = new Sale();
+        sale.setGid(goodsId);
+        sale.setNums(amount);
+        saleDao.insertSale(sale);
+
+        Goods goods = goodsDao.selectGoods(goodsId);
+        if (goods == null) {
+            throw new NullPointerException("无此商品");
+        }
+        if (goods.getAmount() < amount){
+            throw new NotEnoughException("库存不足");
+        }
+        goods = new Goods();
+        goods.setAmount(amount);
+        goods.setId(goodsId);
+        goodsDao.updateGoods(goods);
+
+    }
+}
