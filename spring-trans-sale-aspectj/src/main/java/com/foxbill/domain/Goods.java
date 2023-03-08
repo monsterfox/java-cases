fa2877d996fde1ccc30e2fac7f8e1c978589db2e
foxbill <565197135@qq.com>
2023-03-08 09:56:42 +0800

    spring-基于Aspectj处理事务

---
+++ spring-trans-sale-aspectj/src/main/java/com/foxbill/domain/Goods.java
@@ -0,0 +1,12 @@
+package com.foxbill.domain;
+
+import lombok.Data;
+
+@Data
+public class Goods {
+    private Integer id;
+    private String name;
+    private Integer amount;
+    private float price;
+
+}
