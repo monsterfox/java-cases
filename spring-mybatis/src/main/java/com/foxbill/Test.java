d51b31b9b7abc17e4a6ccccc2d30127a85464ed0
foxbill <565197135@qq.com>
2023-03-17 20:08:34 +0800

    spring-ssm:修改配置文件的注释

---
+++ spring-mybatis/src/main/java/com/foxbill/Test.java
@@ -0,0 +1,8 @@
+package com.foxbill;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(Test.class.getClassLoader());
+        System.out.println(String.class.getClassLoader());
+    }
+}
