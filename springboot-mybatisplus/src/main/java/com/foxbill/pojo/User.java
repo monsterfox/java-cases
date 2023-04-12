64ec7bc9ea3971cd305a7a1e83e8f5364c873642
foxbill <565197135@qq.com>
2023-04-13 07:49:45 +0800

    SpringBoot-mybatisplus:整合mybatisplus模板

--- springboot-mybatisplus/src/main/java/com/foxbill/pojo/User.java
+++ springboot-mybatisplus/src/main/java/com/foxbill/pojo/User.java
@@ -1,19 +1,26 @@
 package com.foxbill.pojo;
 
+import com.baomidou.mybatisplus.annotation.Version;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
-    private Integer id;
+    private Long id;
+//    @Builder.Default
+//    private String name = "洛克菲勒";
     private String name;
     private Integer age;
     private double balance;
+    @Version
+    private Integer version;
 
-    public User(Integer id, double balance) {
+    public User(Long id, double balance) {
         this.id = id;
         this.balance = balance;
     }
