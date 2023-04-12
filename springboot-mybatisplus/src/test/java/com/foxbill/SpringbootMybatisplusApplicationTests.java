b153d92653e786c59a6f27266039b2ddb0f40c65
foxbill <565197135@qq.com>
2023-04-12 11:49:17 +0800

    springBoot整合MyBatis-Plus

--- springboot-mybatisplus/src/test/java/com/foxbill/SpringbootMybatisplusApplicationTests.java
+++ springboot-mybatisplus/src/test/java/com/foxbill/SpringbootMybatisplusApplicationTests.java
@@ -1,8 +1,12 @@
 package com.foxbill;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
+import com.baomidou.mybatisplus.core.toolkit.support.SFunction;
 import com.foxbill.mapper.UserMapper;
 import com.foxbill.pojo.User;
 import com.foxbill.service.UserService;
+import com.foxbill.util.MPLambdaWrapperUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -28,6 +32,23 @@ class SpringbootMybatisplusApplicationTests {
 	void testService(){
 		List<User> list = userService.list();
 		list.forEach(System.out::println);
+	}
+
+	@Test
+	public void testLambda01(){
+		String username = "高";
+		Integer ageBegin = 10;
+		Integer ageEnd = 24;
+		LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<>();
+		queryWrapper.like(StringUtils.isNotBlank(username),User::getName,username)
+					.ge(ageBegin!=null,User::getAge,ageBegin)
+					.le(ageEnd!=null,User::getAge,ageEnd);
+
+		System.out.println(MPLambdaWrapperUtil.getColumn(User::getName));
+		System.out.println(MPLambdaWrapperUtil.getColumn(User::getAge));
+
+		List<User> users = userMapper.selectList(queryWrapper);
+		users.forEach(System.out::println);
 	}
 
 }
