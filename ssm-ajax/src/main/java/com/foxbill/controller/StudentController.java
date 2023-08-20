0b5d09509178fd1810abbec7621499fe34476301
foxdell <565197135@qq.com>
2023-08-20 14:40:52 +0800

    ssm-ajax：通过html+ajax（axios）实现crud.(抛弃jsp)

--- ssm-ajax/src/main/java/com/foxbill/controller/StudentController.java
+++ ssm-ajax/src/main/java/com/foxbill/controller/StudentController.java
@@ -5,6 +5,7 @@ import com.foxbill.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -19,41 +20,39 @@ public class StudentController {
     /*设置首页*/
     @RequestMapping("/")
     public String toList(){
-        return "redirect:static/list.html";
+        return "redirect:static/list-axios.html";
     }
 
     @RequestMapping("/findAll")
     @ResponseBody  //返回json
     public Object findAllStudents(){
         List<Student> list = studentService.findAllStudents();
-
         return list;
     }
 
     @RequestMapping("/doAdd")
-    public String doAdd(Student student){
+    @ResponseBody
+    public void doAdd(@RequestBody Student student){
         studentService.addStudent(student);
-        return "redirect:static/list.html";
     }
 
     @RequestMapping("/toUpt")
     @ResponseBody
-    public Object toUpt(int id,Model model){
+    public Object toUpt(int id){
         Student student = studentService.findStudentById(id);
-        model.addAttribute("student",student);
         return student;
     }
 
     @RequestMapping("/doUpt")
-    public String doUpt(Student student){
+    @ResponseBody
+    public void doUpt(@RequestBody Student student){
         studentService.modifyStudent(student);
-        return "redirect:static/list.html";
     }
 
     @RequestMapping("/doDel")
-    public String doDel(int id){
+    @ResponseBody
+    public void doDel(int id){
         studentService.removeStudent(id);
-        return "redirect:static/list.html";
     }
 
 }
